221fcde277cf5183715b3cedfd7276d2a0fffe85
filip667 <126720220+filip667@users.noreply.github.com>
2024-04-24 21:28:59 +0200

    Add files via upload

---
+++ UpdateImagePrimaryCommand/UpdateImagePrimaryCommand.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using PersonalStudio.Application.Domain.Entities;
+using PersonalStudio.Application.Domain.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PersonalStudio.Application.Services.Commands.UpdateImagePrimaryCommand
+{
+    public class UpdateImagePrimaryCommand : IRequest<Unit>
+    {
+        public Guid ImageId { get; set; }
+    }
+
+    public class ImagePrimaryCommandHandler : IRequestHandler<UpdateImagePrimaryCommand, Unit>
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ImagePrimaryCommandHandler(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateImagePrimaryCommand request, CancellationToken cancellationToken)
+        {
+            // Pobierz obrazek z repozytorium na podstawie przekazanego ID
+            var image = await _imageRepository.Get(request.ImageId);
+            if (image == null)
+            {
+                throw new Exception("Image not found");
+            }
+
+            // Pobierz wszystkie obrazy dla produktu
+            var imagesForProduct = await _imageRepository.GetImagesByProductId(image.ProductId);
+
+            // Zmodyfikuj flagi IsPrimary dla obrazków
+            foreach (var img in imagesForProduct)
+            {
+                img.IsPrimary = (img.Id == request.ImageId); // Ustaw na true tylko dla obrazka, który ma być ustawiony jako główny
+                await _imageRepository.UpdateImage(img);
+            }
+#cokolwiek
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ UpdateImagePrimaryCommand/UpdateImagePrimaryCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using PersonalStudio.Application.Services.Commands.UpdateImagePrimaryCommand;
+
+public class UpdateImagePrimaryCommandValidator : AbstractValidator<UpdateImagePrimaryCommand>
+{
+    public UpdateImagePrimaryCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty()
+            .WithMessage("Image Id is required.");
+    }
+}
